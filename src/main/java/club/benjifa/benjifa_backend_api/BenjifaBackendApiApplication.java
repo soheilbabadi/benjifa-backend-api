9c32f1396f4868e70094a80d8499bdb83e43b1ff
soheil babadi <babadishyl@gmail.com>
2025-03-24 08:21:49 +0330

    init commit

---
+++ src/main/java/club/benjifa/benjifa_backend_api/BenjifaBackendApiApplication.java
@@ -0,0 +1,13 @@
+package club.benjifa.benjifa_backend_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenjifaBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenjifaBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/club/benjifa/benjifa_backend_api/BenjifaBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package club.benjifa.benjifa_backend_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenjifaBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
